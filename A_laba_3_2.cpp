af12a63ccd1ef6075b52a512f562fe9a75d0f0fd
Daniil <150480445+S-daniil-I@users.noreply.github.com>
2023-11-25 11:58:05 +0300

    Update A_laba_3_2.cpp

--- A_laba_3_2.cpp
+++ A_laba_3_2.cpp
@@ -1,6 +1,7 @@
 //Найти все такие числа k, что k - я строка совпадает с k - м столбцом.Найти
 //наибольший элемент главной диагонали матрицы и вывести на печать строку, в
 //которой он находится.
+
 #include <iostream>
 #include <iomanip>
 #include <math.h>
@@ -12,7 +13,10 @@ int main()
     setlocale(LC_ALL, "Ru");
     int a, max, count = 0, n = 0;
     const int N = 4;
-
+    int arr[N][N]{ {10,2,3,5},
+                   {3,9,5,4},
+                   {5,5,5,7},
+                   {5,4,7,5}};;
 
     enum ch { const_numb, user_numb, rand_numb };
 
@@ -31,114 +35,56 @@ int main()
 
     switch (choice)
     {
-    case const_numb: {
-        int arr[N][N] = { {10,2,3,5},
-                          {3,9,5,4},
-                          {5,5,5,7},
-                          {5,4,7,5} };
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                cout << setw(3) << arr[i][j];
-            cout << endl;
-        }
-        max = arr[0][0];
-        int k = 0;
-        for (int i = 0; i < N; i++) {
-
-            if (arr[i][i] > max) {
-                max = arr[i][i];
-                k = i;
-
-            }
-        }
-        cout << "Максимальный элемент равен:" << max << " " << ", находится на " << k + 1 << "-ой строке \n";
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == arr[j][i])
-                    count++;
-                if (count == N) {
-                    cout << i + 1 << " " << "строка совпдает с " << j + 1 << " cтолбцом";
-                    n++;
-                }
-            }
-            count = 0;
-        }
-        if (n == 0)
-            cout << "Совпадений не найдено";
+    case const_numb: { 
+       
         break; }
     case user_numb: {
-        int arr[N][N];
+        cout << "Введите " << N * N << " элементов матрицы! \n";
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 cout << "Введите элементы матрицы: ";
                 cin >> arr[i][j];
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                cout << setw(3) << arr[i][j];
-            cout << endl;
-        }
-        max = arr[0][0];
-        int k = 0;
-        for (int i = 0; i < N; i++) {
-
-            if (arr[i][i] > max) {
-                max = arr[i][i];
-                k = i;
-            }
-        }
-        cout << "Максимальный элемент равен:" << max << " " << ", находится на " << k + 1 << "-ой строке \n";
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == arr[j][i])
-                    count++;
-                if (count == N) {
-                    cout << i + 1 << " " << "строка совпдает с " << j + 1 << " cтолбцом";
-                    n++;
-                }
-            }
-            count = 0;
-        }
-        if (n == 0)
-            cout << "Совпадений не найдено";
         break; }
     case rand_numb: {
         srand(time(nullptr));
-        int arr[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 arr[i][j] = rand() % 10;
-                cout << setw(3) << arr[i][j];
-            }
-            cout << endl;
-        }
-        max = arr[0][0];
-        int k = 0;
-        for (int i = 0; i < N; i++) {
-            if (arr[i][i] > max) {
-                max = arr[i][i];
-                k = i;
             }
         }
-        cout << "Максимальный элемент равен:" << max << " " << ", находится на " << k + 1 << "-ой строке \n";
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == arr[j][i])
-                    count++;
-                if (count == N) {
-                    cout << i + 1 << " " << "строка совпдает с " << j + 1 << " cтолбцом";
-                    n++;
-                }
-            }
-            count = 0;
-        }
-        if (n == 0)
-            cout << "Совпадений не найдено";
         break; }
     default:
         cout << "Что-то пошло не так\n";
         break;
     }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << setw(3) << arr[i][j];
+        cout << endl;
+    }
+    max = arr[0][0];
+    int k = 0;
+    for (int i = 0; i < N; i++) {
+        if (arr[i][i] > max) {
+            max = arr[i][i];
+            k = i;
+        }
+    }
+    cout << "Максимальный элемент равен:" << max << " " << ", находится на " << k + 1 << "-ой строке \n";
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (arr[i][j] == arr[j][i])
+                count++;
+            if (count == N) {
+                cout << i + 1 << " " << "строка совпдает с " << j + 1 << " cтолбцом";
+                n++;
+            }
+        }
+        count = 0;
+    }
+    if (n == 0)
+        cout << "Совпадений не найдено";
     return 0;
 }
