69f21114cf796ea88449e76052ebad9b3586bdce
Daniil <150480445+S-daniil-I@users.noreply.github.com>
2023-11-25 12:18:06 +0300

    Add A_laba_2_7
    A_laba_2_7 made without data storage

---
+++ A_laba_2_7.cpp
@@ -0,0 +1,28 @@
+﻿//Вводится последовательность из n вещественных чисел.Найти порядковый
+//номер того из них, которое наиболее близко к какому - либо целому.
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n,index=0;
+	double comparison_number=1;
+	double number;
+	int integer_part = 0;
+	cout << "Введите количество элементов:";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << "Введите " << i << "-e число: ";
+			cin >> number;
+			integer_part = round(number);
+			if (abs(number - integer_part) < comparison_number) {
+				index = i;
+				comparison_number = abs(number - integer_part);
+			}
+	}
+	cout <<"Номер элемента: " << index;
+	return 0;
+	
+}
